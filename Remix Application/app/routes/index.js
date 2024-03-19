654b17031c6e33b4651d4e039fd8c58e3f84c3e5
annarm994 <155604262+annarm994@users.noreply.github.com>
2024-03-19 18:41:14 +0500

    Add files via upload

---
+++ Remix Application/app/routes/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Index() {
+  return (
+    <div>
+      <h1>Welcome to My Remix App!</h1>
+      <p>This is the homepage of the Remix app.</p>
+    </div>
+  );
+}
